605369450b427a5cc326f6aa1ad2a75e9eed085e
harun-kamande <kamandeharun99@gmail.com>
2025-08-20 22:17:27 +0300

    changes

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Users, Flower, Order
-from .Hash_password import hash_password
+from .hash_password import hash_password
 
 # Create your views here.
 def home(request):
@@ -30,18 +30,36 @@ def login(request):
     #         return HttpResponse("Invalid credentials")
     if request.method == "GET":
         return render(request, 'login.html')
-        
-    return render(request, 'login.html')
+    
+    elif request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        hashed_password = hash_password(password)
+
+        user=Users.objects.filter(email=email).first()
+
+        if user:
+            return HttpResponse("User already exists")
+        else:
+            user = Users.objects.filter(email=email, password=hashed_password).first()
+            if user:
+                request.session['user_id'] = user.id
+                return render(request, 'home.html', {'user': user})
+            else:
+                return HttpResponse("Invalid credentials")
+
 
 def create(request):
     if request.method == "GET":
         return render(request, 'create_account.html')
     elif request.method == "POST":
+        username = request.POST.get('username')
+        profile_photo = request.FILES.get('profile_photo')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        hashed_password = hash_password.hash_password(password)
+        hashed_password = hash_password(password)
 
-        user = Users(email=email, password=hashed_password)
+        user = Users(email=email, password=hashed_password, username=username, profile_photo=profile_photo)
         user.save()
         return render(request, 'login.html')
     return render(request, 'create_account.html')
